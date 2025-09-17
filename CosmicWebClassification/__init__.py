51a1d4b261b187bdea749d1571e7c8169ea507c2
jwinstan <jwin0686@uni.sydney.edu.au>
2025-09-17 16:03:30 +1000

    Update __init__.py

--- CosmicWebClassification/__init__.py
+++ CosmicWebClassification/__init__.py
@@ -1,1 +1,46 @@
 from .cosmic_web import *
+
+
+'''from .cosmic_web import (
+    # Main class
+    CosmicWebClassifier,
+
+    # NumPy versions
+    build_mass_grid,
+    build_velocity_grid,
+    compute_shear_tensor,
+    diagonalize_shear_tensor,
+    classify_cosmic_web,
+    apply_multiscale_correction,
+
+    # Numba kernels (mass)
+    build_mass_grid_numba,
+
+    # Numba kernels (velocity)
+
+    build_velocity_grid_numba,
+
+    compute_average_velocity,
+    compute_density_grid
+)
+
+__all__ = [
+    # Main class
+    "CosmicWebClassifier",
+
+    # NumPy versions
+    "build_mass_grid",
+    "build_velocity_grid",
+    "compute_shear_tensor",
+    "diagonalize_shear_tensor",
+    "classify_cosmic_web",
+    "apply_multiscale_correction",
+
+    # Numba kernels (mass)
+    "build_mass_grid_numba",
+
+    # Numba kernels (velocity)
+    "build_velocity_grid_numba",
+    "compute_average_velocity",
+    "compute_density_grid"
+]'''
